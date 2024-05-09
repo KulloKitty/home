090e717f01f68e22c2889748e1b38e78b160d6fe
KulloKitty <164616934+KulloKitty@users.noreply.github.com>
2024-05-09 21:17:15 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Windows } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, Gem, Cat, Dragon, Poo, Fire, YinYang, Windows, BattleNet } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -65,12 +65,13 @@ const siteLinksList = computed(() => {
 const siteIcon = {
   Blog,
   Windows,
-  Cloud,
-  CompactDisc,
-  Compass,
-  Book,
+  Gem, 
+  Cat, 
+  Dragon, 
+  Poo,
+  YinYang,  
+  BattleNet,
   Fire,
-  LaptopCode,
 };
 
 // 链接跳转
