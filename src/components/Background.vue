01b937f4bd7a2df8a6e9a306d84abaed625ffbea
KulloKitty <164616934+KulloKitty@users.noreply.github.com>
2024-05-11 12:56:42 +0800

    Update Background.vue

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -43,7 +43,7 @@ const changeBg = (type) => {
   } else if (type == 1) {
     bgUrl.value = "https://api.dujin.org/bing/1920.php";
   } else if (type == 2) {
-    bgUrl.value = "https://source.unsplash.com/user/kullokitty/likes/2560×1440";
+    bgUrl.value = "https://source.unsplash.com/random/2560×1440";
   } else if (type == 3) {
     bgUrl.value = "https://api.dujin.org/pic/";
   }
